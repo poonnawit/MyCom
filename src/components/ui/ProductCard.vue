49e114bdf4de9a23e22d8361633f056bfcc25310
Poonnawit <poonnawit@outlook.com>
2022-08-23 23:36:51 +0700

    Grid & List toggle btn

--- src/components/ui/ProductCard.vue
+++ src/components/ui/ProductCard.vue
@@ -10,13 +10,26 @@ const props = defineProps({
     required: false,
     default: "Lorem ipsum dolor sit amet consectetur.",
   },
+  summary: {
+    type: String,
+    required: false,
+    default: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, aliquam!",
+  },
+  mode: {
+    type: String,
+    required: false,
+    default: "grid",
+  },
 });
 </script>
 
 <template>
-  <article class="bg-alternative rounded-md overflow-hidden">
+  <article v-if="mode === 'grid'" class="bg-alternative rounded-md overflow-hidden">
     <router-link to="#"
-      ><img :src="props.imgSource" alt="product" class="transition duration-500 w-full h-60 object-cover hover:scale-105"
+      ><img
+        :src="props.imgSource"
+        alt="product"
+        class="transition duration-500 w-full h-60 object-cover hover:scale-105"
     /></router-link>
     <div class="p-4">
       <h2 class="transition font-bold hover:text-alternativebg">
@@ -26,6 +39,29 @@ const props = defineProps({
         <h3 class="font-bold text-xl">4,200.-</h3>
         <button class="btn-primary text-sm">Add to cart</button>
       </div>
+    </div>
+  </article>
+  <article v-if="mode === 'list'" class="bg-alternative rounded-md overflow-hidden">
+    <h2>{{ props.title }}</h2>
+    <p>{{ props.summary }}</p>
+    <h2>4,200.-</h2>
+    <div>
+      <button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+          />
+        </svg>
+      </button>
     </div>
   </article>
 </template>
--- src/views/customer/ProductsList.vue
+++ src/views/customer/ProductsList.vue
@@ -1,11 +1,75 @@
 <script setup>
-import SidebarCategory from '../../components/layout/SidebarCategory.vue';
+import { ref } from "@vue/reactivity";
+import { watch } from "@vue/runtime-core";
+import SidebarCategory from "../../components/layout/SidebarCategory.vue";
+
+const productsViewMode = ref("grid");
 </script>
 
 <template>
   <div class="flex">
     <SidebarCategory></SidebarCategory>
     <section>
+      <h1 class="text-center font-bold text-2xl my-10">Products List</h1>
+      <ul class="flex items-center w-fit mx-auto my-5 border border-primary rounded-md">
+        <li class="flex">
+          <input
+            type="radio"
+            name="views-toggle"
+            id="grid"
+            class="hidden peer"
+            value="grid"
+            v-model="productsViewMode"
+            checked
+          />
+          <label
+            for="grid"
+            class="transition px-2 py-1 rounded-l-md cursor-pointer hover:bg-alternative peer-checked:bg-alternativebg peer-checked:text-white"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+              />
+            </svg>
+          </label>
+        </li>
+        <li class="flex">
+          <input
+            type="radio"
+            name="views-toggle"
+            id="list"
+            class="hidden peer"
+            value="list"
+            v-model="productsViewMode"
+          />
+          <label
+            for="list"
+            class="transition px-2 py-1 rounded-r-md cursor-pointer hover:bg-alternative peer-checked:bg-alternativebg peer-checked:text-white"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4 6h16M4 10h16M4 14h16M4 18h16"
+              />
+            </svg>
+          </label>
+        </li>
+      </ul>
       <ul class="grid grid-cols-4 gap-2 my-2 mx-auto">
         <li>
           <ProductCard imgSource="/img/A0141528OK_BIG_1.jpg"></ProductCard>
